7119ed52dcc981e6d32637497d30f837a1aff16a
Ahmadreza Zibaei <ahmadrezazibaei@hotmail.com>
2021-06-27 15:31:14 +0430

    add ability to change net directory

--- deep_hazmat/deep_hazmat.py
+++ deep_hazmat/deep_hazmat.py
@@ -1,17 +1,16 @@
-from os.path import join, dirname
+from os.path import join
 
 from deep_hazmat.detector import YoloDetection
 from deep_hazmat.feeding import ImageFeedingOptimisation
 
 
 class DeepHAZMAT:
-    NET_DIRECTORY = join(dirname(__file__), 'net')
-
-    def __init__(self, k, min_confidence=0.8, nms_threshold=0.3, segmentation_enabled=True):
+    def __init__(self, k, net_directory, min_confidence=0.8, nms_threshold=0.3,
+                 segmentation_enabled=True):
         self._detector = YoloDetection(
-            weights=join(self.NET_DIRECTORY, 'yolo.weights'),
-            config=join(self.NET_DIRECTORY, 'yolo.cfg'),
-            labels=join(self.NET_DIRECTORY, 'labels.names'),
+            weights=join(net_directory, 'yolo.weights'),
+            config=join(net_directory, 'yolo.cfg'),
+            labels=join(net_directory, 'labels.names'),
             input_size=(576, 576),
             min_confidence=min_confidence,
             nms_threshold=nms_threshold,
